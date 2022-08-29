893b6e8df901f01cfdb12cee3e360bf5aef2437b
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-08-30 01:06:22 +0300

    1st

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const allCategoriesItems = document.querySelectorAll(".item");
+console.log("Number of categories:", allCategoriesItems.length);
+
+const Category = document.querySelector("h2").textContent;
+console.log("Category:", Category);
+
+const Elements = document.querySelector("ul ");
+console.log("Elements:", Elements.length);
