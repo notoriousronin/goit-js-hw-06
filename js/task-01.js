d359ece649317d283c0e0d939fab9ba58f0641d5
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-08-30 13:21:59 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,7 @@ const allCategoriesItems = document.querySelectorAll(".item");
 console.log("Number of Categories:", allCategoriesItems.length);
 
 allCategoriesItems.forEach((item) => {
-  const titleEl = item.querySelector("h2");
-  console.log("Category:", titleEl.textContent);
+  console.log("Category:", item.firstElementChild.textContent);
 
   const Elements = item.querySelectorAll("ul > li");
   console.log("Elements:", Elements.length);
