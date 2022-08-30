ad05ca0d91ad3ca874020143a6d16a83f55497ff
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-08-30 19:16:54 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsPath = document.querySelector("#ingredients");
+
+const arr = ingredients.map((element) => {
+  const newEl = document.createElement("li");
+  newEl.classList.add("item");
+  newEl.textContent = element;
+
+  return newEl;
+});
+
+ingredientsPath.append(...arr);
