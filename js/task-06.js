da32c860f8617b72485c0d4768a9bd405528f06a
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-01 23:10:55 +0300

    6

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const refs = {
+  input: document.querySelector("#validation-input"),
+};
+
+refs.input.addEventListener("blur", () => {
+  if (refs.input.value.length === Number(refs.input.dataset.length)) {
+    refs.input.classList.add("valid");
+    refs.input.classList.remove("invalid");
+  } else {
+    refs.input.classList.remove("valid");
+    refs.input.classList.add("invalid");
+  }
+});
