ae44958f61d614f2d3cd5b030418768e6cff0554
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-02 14:59:47 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please, enter your information");
+  }
+  console.log("Email: ${email.value}, Password: ${password.value}");
+  event.currentTarget.reset();
+
+  //   const formData = new FormData(event.currentTarget);
+
+  //   formData.forEach((value, name) => {
+  //     console.log("onFormSubmit -> name", name);
+  //     console.log("onFormSubmit -> value", value);
+  //   });
+}
