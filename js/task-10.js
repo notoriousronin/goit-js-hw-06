bfd4ed6ea7bea1cc1805cb437404ed51cda23ce9
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-02 20:23:27 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const inputEl = document.querySelector('[type="number"]');
+const boxesEl = document.querySelector("#boxes");
+
+const createBoxes = () => {
+  const amount = Number(inputEl.value);
+  const elementSize = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const newSize = elementSize + i * 10;
+    const divEl = document.createElement("div");
+    divEl.style.width = `${newSize}px`;
+    divEl.style.height = `${newSize}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(divEl);
+  }
+};
+
+const destroyBoxes = () => {
+  boxesEl.innerHTML = "";
+};
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
