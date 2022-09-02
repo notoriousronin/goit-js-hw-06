9bfe03890194d957644b19e8b3d56fb552ff2ff6
Andrey Gavrilyuk <andrew250519@gmail.com>
2022-09-02 19:59:44 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const span = document.querySelector(".color");
+const changeClrBtn = document.querySelector(".change-color");
+
+changeClrBtn.addEventListener("click", () => {
+  const currentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentColor;
+  span.textContent = currentColor;
+});
